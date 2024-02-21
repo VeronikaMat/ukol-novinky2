006d11317c99995c3aa2dc344204c8ee9da7ec07
Veronika Matoušková <andrlovaver@gmail.com>
2024-02-21 19:19:30 +0100

    add code

---
+++ index.js
@@ -0,0 +1,12 @@
+const bodyElement = document.querySelector("body")
+bodyElement.style.backgroundColor = '#e9e9e9'
+
+const newsElement = document.querySelector(".news")
+newsElement.style.backgroundColor = "white"
+newsElement.style.maxWidth = "60rem"
+
+const h1Element = document.querySelector("h1")
+h1Element.className = "news__title"
+h1Element.textContent = "Aktualni novinky"
+
+
